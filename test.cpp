72cb3afea861c0fa262db08a0cf3d3c7c6f4a8bd
EvelynWu1012 <evelyn.qianqian@gmail.com>
2024-12-31 14:05:51 +0000

    C++ basic knowledge

---
+++ test.cpp
@@ -0,0 +1,19 @@
+#include <iostream> 
+// #include is the input statement
+// io stands for input and output
+using namespace std;
+// The line using namespace std; in C++ is used to bring all the symbols (like classes, functions, objects, etc.) 
+// from the std namespace into the global namespace, making them directly accessible without the need to explicitly qualify them with std::.
+
+// int in the front of main() means that the function returns a int type
+ int main(){
+    // cout: mean console out, it is a output stream
+    // << >> output operator
+    // endl; mean end line
+    cout << "Hello Qianqian!" << endl << "Kenny";
+    // return 0;
+    // ; semicolon tells the compiler when the line ends
+}
+/*
+This is a block comment
+*/
